679bb2826a2600b7c2a2361f3b1c3657f91c10be
doting1 <doting059@gmail.com>
2023-10-30 16:29:31 +0700

    tich

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	return a * b;
+}
 float thuong(int a, int b);
 int main()
 {
